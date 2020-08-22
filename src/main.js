08a9dfdead1ad788ae8308f1fdfb8c2324ecafb0
flosacca <flosacca@gmail.com>
2020-08-22 20:57:05 +0800

    Add axios

--- src/main.js
+++ src/main.js
@@ -3,10 +3,14 @@ import Antd from 'ant-design-vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueAxios from 'vue-axios'
+import axios from 'axios'
 
 import 'ant-design-vue/dist/antd.css'
 
 Vue.config.productionTip = false
+
+Vue.use(VueAxios, axios)
 
 Vue.use(Antd)
 
