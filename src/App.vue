28c6c3b03fa16f9ddc2792f1ef91db52df590531
flosacca <flosacca@gmail.com>
2020-08-22 15:30:38 +0800

    Add layout

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,15 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <a-layout id="layout">
+    <a-layout-header class="header">
+      <div class="logo">bbs</div>
+      <a-button class="login">log in</a-button>
+    </a-layout-header>
+    <a-layout-content class="content">
+      <div class="center">
+        <router-view/>
+      </div>
+    </a-layout-content>
+  </a-layout>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+<style lang="scss" src="./App.vue.scss"/>
