3710b0cca2a795c93be881d6feeee6deb7439e9e
flosacca <flosacca@gmail.com>
2020-08-23 02:48:17 +0800

    Fixup

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <a-layout id="layout">
-    <a-layout-header class="header">
-      <div class="logo">bbs</div>
-      <a-button class="login">log in</a-button>
-    </a-layout-header>
-    <a-layout-content class="content">
-      <div class="center">
-        <router-view/>
-      </div>
-    </a-layout-content>
-  </a-layout>
+  <div id="app">
+    <a-layout>
+      <a-layout-header class="header">
+        <div class="logo">bbs</div>
+        <a-button class="login">log in</a-button>
+      </a-layout-header>
+      <a-layout-content class="content">
+        <div class="center">
+          <router-view/>
+        </div>
+      </a-layout-content>
+    </a-layout>
+  </div>
 </template>
 
 <style lang="scss" src="./App.vue.scss"/>
