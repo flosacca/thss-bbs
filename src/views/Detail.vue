fe04ae9663891567eb87af52206ce37c17550cad
flosacca <flosacca@gmail.com>
2020-08-29 04:37:32 +0800

    Add edit back

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="post" class="view">
-    <h1>{{ post.title }}</h1>
+    <h1 v-if="editing.id !== 0">
+      {{ post.title }}
+    </h1>
+    <a-input v-else
+      v-model="editForm.title"
+    />
     <a-list
       item-layout="vertical"
       :loading="loading"
@@ -17,21 +22,34 @@
               / <a @click="edit(reply)">edit</a>
             </span>
           </div>
-          <div v-if="reply.replyId != null">
-            &gt;&gt;{{ reply.replyId }}
+          <div v-if="reply.replyId == null && editing.id === 0">
+            <editor-form
+              ref="editForm"
+              :form="editForm"
+              :hasTitle="false"
+              submitText="edit post"
+              :submitting="postSending"
+              @submit="editPost"
+            />
           </div>
-          <div v-html="render(reply)"></div>
+          <div v-else>
+            <div v-if="reply.replyId != null">
+              &gt;&gt;{{ reply.replyId }}
+            </div>
+            <div v-html="render(reply)"></div>
+          </div>
         </a-list-item>
       </template>
     </a-list>
 
     <editor-form
-      ref="form"
-      :form="form"
+      ref="replyForm"
+      :form="replyForm"
       :hasTitle="false"
       submitText="new reply"
-      :submitting="posting"
-      @submit="submit"
+      :submitting="replying"
+      @submit="newReply"
+      v-if="hasReply"
     />
   </div>
 </template>
@@ -50,10 +68,15 @@ export default {
   data() {
     return {
       loading: true,
-      editing: false,
-      posting: false,
+      replying: false,
+      postSending: false,
+      editing: {},
       post: {},
-      form: {
+      replyForm: {
+        content: ''
+      },
+      editForm: {
+        title: '',
         content: ''
       }
     }
@@ -80,19 +103,35 @@ export default {
     },
 
     edit(reply) {
+      if (reply.replyId == null) {
+        this.editing = { id: 0 }
+        let { title, content } = this.post
+        this.editForm = { title, content }
+      }
     },
 
-    async submit() {
-      console.log('new reply', this.form)
-      this.posting = true
+    async editPost() {
+      console.log('edit post', this.editForm)
+      this.postSending = true
+      await this.req(`/post/${this.id}`, {
+        method: 'put',
+        data: this.editForm
+      })
+      this.postSending = false
+      this.reload()
+    },
+
+    async newReply() {
+      console.log('new reply', this.replyForm)
+      this.replying = true
       await this.req(`/post/${this.id}/reply`, {
         method: 'post',
         data: {
-          ...this.form,
+          ...this.replyForm,
           replyId: 0
         }
       })
-      this.posting = false
+      this.replying = false
       this.reload()
     }
   }
